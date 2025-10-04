68f94d1b36e0f5edfa8b3f3d554d967aedecf8fd
Edis Leka <edisleka@yahoo.gr>
2025-10-04 17:19:38 +0300

    app structure modified

---
+++ src/app/(onboarding)/onboarding.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function OnboardingScreen() {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-xl font-bold text-blue-500'>Onboarding Screen</Text>
+    </View>
+  )
+}
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,5 +2,23 @@ import '@root/global.css'
 import { Stack } from 'expo-router'
 
 export default function RootLayout() {
-  return <Stack />
+  const showOnboarding = true
+  const showAuth = false
+  const showProtected = false
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={showOnboarding}>
+        <Stack.Screen name='(onboarding)' />
+      </Stack.Protected>
+
+      <Stack.Protected guard={showAuth}>
+        <Stack.Screen name='(auth)' />
+      </Stack.Protected>
+
+      <Stack.Protected guard={showProtected}>
+        <Stack.Screen name='(protected)' />
+      </Stack.Protected>
+    </Stack>
+  )
 }
